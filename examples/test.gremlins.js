bee8f632441ac090f464d414cc35e2e3ed44acd4
Alexey.Kupriyanenko <alexey.kupriyanenko@playtech.com>
2014-03-21 12:58:49 +0200

    add screenshot

--- examples/test.gremlins.js
+++
@@ -1,7 +0,0 @@
-var horde = gremlins.createHorde()
-    .gremlin(gremlins.species.clicker())
-    .gremlin(gremlins.species.scroller())
-    .mogwai(gremlins.mogwais.gizmo())
-    .mogwai(gremlins.mogwais.fps());
-
-window.phantomHorde && window.phantomHorde(horde, { nb: 20 });
